3db9899d7b0d78b915dc1874c5eaf0b3dc9203ce
Charles Horton <charlie@horton5.com>
2025-07-28 08:11:48 -0700

    wip

--- src/main/kotlin/devtools/FileBrowser.kt
+++ src/main/kotlin/devtools/FileBrowser.kt
@@ -1,13 +1,56 @@
 package org.kotlingl.devtools
 
-import imgui.type.ImBoolean
+import imgui.ImGui.*
+import imgui.ImVec2
+import imgui.flag.ImGuiCond
+import imgui.flag.ImGuiWindowFlags
+import imgui.type.ImString
+import java.nio.file.Paths
 
 
-
-class FileBrowser(var path: String): Window() {
+class FileBrowser(
+    var path: String,
+    val title: String,
+    val extensions: List<String>,
+): Window() {
     val defaultPath = path
+    val currentPath = Paths.get(path)
+    val pathInputValue = ImString(path)
+
+    fun upLevel() {
+
+    }
+
+    fun createChildren() {
+
+    }
 
     override fun update() {
-        TODO("Not yet implemented")
+        if (!open.get())
+            return
+
+        val width = 840f
+        val height = 620f
+        val mainViewport = getMainViewport();
+        val xPos = (mainViewport.workSizeX - width)/2
+        val yPos = (mainViewport.workSizeY - height)/2
+        setNextWindowPos(ImVec2(xPos, yPos), ImGuiCond.FirstUseEver);
+        setNextWindowSize(ImVec2(width, height), ImGuiCond.FirstUseEver);
+
+        if (begin(title, this.open, ImGuiWindowFlags.MenuBar)) {
+            inputText("", pathInputValue)
+            sameLine()
+            if (button("up")) { upLevel() }
+
+            beginChild("Files")
+
+            endChild()
+
+            button("cancel")
+            sameLine()
+            button("open")
+
+            end()
+        }
     }
 }
